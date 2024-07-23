3138db7c9f5d161cfccc52fb0b86affa06cd6422
Kuldeep Agrahari <133698085+Kuldeepagrahari@users.noreply.github.com>
2024-07-23 22:03:10 +0530

    chat hub ka update lene aya hu

--- chatHub/client/src/pages/Login.jsx
+++ chatHub/client/src/pages/Login.jsx
@@ -2,9 +2,7 @@ import React, { useState } from 'react'
 import { Paper, Container, Typography, Button, TextField, Stack, Avatar, IconButton } from "@mui/material"
 import { VisuallyHiddenInput } from '../components/styles/styled'
 import { CameraAlt } from "@mui/icons-material"
-const styler = {
-  display: "flex", alignItems: "center", justifyContent: "center", marginTop: "20vh"
-}
+
 const Login = () => {
 
   const [isLogin, setLogin] = useState(true)
@@ -13,12 +11,12 @@ const Login = () => {
 
 
 
-    <Container component={"main"} maxWidth="xs" fullwidth sx={{ display: "flex", alignItems: "center", justifyContent: "center"}}>
-      <Paper elevation={3} sx={{ padding: 4, width: "25vw", display: "flex", flexDirection: "column", alignItems: "center"}}>
+    <Container component={"main"} maxWidth={"xs"}  sx={{ height:"100vh" , display: "flex", alignItems: "center", justifyContent: "center"}}>
+      <Paper elevation={3} sx={{ padding: 4 ,  display: "flex", flexDirection: "column", alignItems: "center"}}>
         {isLogin ?
           <>
-            <Typography sx={{}} variant="h4">Login</Typography>
-            <form style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", width: "100%" }}>
+            <Typography variant="h5">Login</Typography>
+            <form style={{ width: "100%", marginTop:"1rem" }}>
 
               <TextField label="Username" variant='outlined' margin="normal" fullWidth required></TextField>
               <TextField label="Password" type="password" variant='outlined' margin="normal" fullWidth required></TextField>
@@ -30,11 +28,11 @@ const Login = () => {
           </>
           :
           <>
-            <Typography variant="h4" sx={{ marginBottom: "-3vh", marginTop: "-3vh" }}>Register</Typography>
+            <Typography variant="h5" sx={{marginBottom:"10px"}} >Register</Typography>
 
-            <Stack position={"relative"} width={"10rem"} margin={"auto"}>
-              <Avatar sx={{ width: "10rem", height: "10rem", objectFit: "contain" }} />
-              <IconButton>
+            <Stack position={"relative"} width={"7rem"} margin={"auto"}>
+              <Avatar sx={{ width: "7rem", height: "7rem", objectFit: "contain" }} />
+              <IconButton component="label" sx={{position:"absolute", right:0, bottom:0, color:"white", backgroundColor:"rgba(0,0,0,0.5)", ":hover":{backgroundColor:"rgba(0,0,0,0.7)"}}}>
                 <>
                   <CameraAlt />
                   <VisuallyHiddenInput type='file' />
@@ -44,16 +42,16 @@ const Login = () => {
 
 
 
-            <form style={{ width: "100%", marginTop: "1rem" }}>
+            <form style={{ height:"90%" }}>
 
-              <TextField label="Name" variant='outlined' margin="normal" fullWidth required sx={{ marginBottom: "-1vh" }}></TextField>
-              <TextField label="Bio" variant='outlined' margin="normal" fullWidth required sx={{ marginBottom: "-1vh" }}></TextField>
-              <TextField label="Username" variant='outlined' margin="normal" fullWidth required sx={{ marginBottom: "-1vh" }}></TextField>
-              <TextField label="Password" type="password" variant='outlined' margin="normal" fullWidth required sx={{ marginBottom: "-1vh" }}></TextField>
-              <Button variant='contained' type='submit' sx={{ backgroundColor: "green", color: "white", marginTop: "1rem" }} fullWidth margin="normal">Sign In</Button>
+              <TextField  InputProps={{ sx: { height: '40px', '& input': { padding: '0 14px' } } }} label="Name" variant='outlined' margin="normal" fullWidth required sx={{ marginBottom: "-1vh"}}></TextField>
+              <TextField InputProps={{ sx: { height: '40px', '& input': { padding: '0 14px' } } }} label="Bio" variant='outlined' margin="normal" fullWidth required sx={{ marginBottom: "-1vh" }}></TextField>
+              <TextField InputProps={{ sx: { height: '40px', '& input': { padding: '0 14px' } } }} label="Username" variant='outlined' margin="normal" fullWidth required sx={{ marginBottom: "-1vh" }}></TextField>
+              <TextField InputProps={{ sx: { height: '40px', '& input': { padding: '0 14px' } } }} label="Password" type="password" variant='outlined' margin="normal" fullWidth required sx={{ marginBottom: "-1vh" }}></TextField>
+              <Button variant='contained' type='submit' sx={{ backgroundColor: "green", color: "white", marginTop: "1rem" }} fullWidth margin="normal">Sign UP</Button>
 
-              <Typography variant='body1888888' sx={{ marginTop: "1rem", textAlign: "center" }} fullWidth>OR</Typography>
-              <Button onClick={() => setLogin(true)} fullWidth margin="normal" sx={{ marginTop: "0.8rem" }} >Sign In</Button>
+              <Typography variant='body1' textAlign={"center"} sx={{ marginTop: "1rem"}} fullWidth>OR</Typography>
+              <Button onClick={() => setLogin(true)} fullWidth margin="normal" sx={{ marginTop: "0.8rem" }} >Sign IN</Button>
             </form></>
         }
       </Paper>
@@ -63,3 +61,5 @@ const Login = () => {
 }
 
 export default Login
+// -------------------------------
+
